18e478e2c0bde71286dba9b67d11e6633dd1bf4c
vladcantor <rapp93_rod@yahoo.com>
2015-05-29 16:50:01 +0300

    test project added

---
+++ TravellerStudent.Test/Core/BaseTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravellerStudent.Controller.Core;
+
+namespace TravellerStudent.Test.Core
+{
+    public class BaseTest
+    {
+        protected ControllerContext _context;
+
+        public BaseTest()
+        {
+            _context = new ControllerContext(new Model.Core.BaseContext());
+        }
+    }
+}
---
+++ TravellerStudent.Test/UserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravellerStudent.Test.Core;
+
+namespace TravellerStudent.Test
+{
+    [TestClass]
+    public class UserTests:BaseTest
+    {
+        [TestMethod]
+        public void TestAddUser()
+        {
+           //_context.Controller.User
+        }
+    }
+}
