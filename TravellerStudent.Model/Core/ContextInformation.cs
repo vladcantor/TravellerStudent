a4824facdd0e886c6040198407aa5da4e59d3449
Vlad Cantor <rapp93_rod@yahoo.com>
2015-06-05 18:01:33 +0300

    excluded files

---
+++ TravellerStudent.Model/Core/ContextInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TravellerStudent.Model.Core
+{
+    public class ContextInformation
+    {
+        public string CurrentUsername { get; set; }
+    }
+}
